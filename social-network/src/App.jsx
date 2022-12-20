a5a20d67e8ceb60c2d7ed103d55670fe2e3abe61
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-20 19:27:59 +0400

    structure-preparation

---
+++ social-network/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import logo from "./img/header/logo.svg";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} width="24" height="24" />
+      </header>
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
