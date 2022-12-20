dbf2ba4fc644ed66182751e473ff2b097077f45b
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-20 19:43:59 +0400

    added-null-style

--- social-network/src/App.jsx
+++ social-network/src/App.jsx
@@ -2,19 +2,20 @@ import "./App.css";
 import logo from "./img/header/logo.svg";
 function App() {
   return (
-    <div className="App">
+    <div className="app-wrapper">
       <header className="App-header">
         <img src={logo} width="24" height="24" />
       </header>
       <nav>
         <ul>
-          <li></li>
+          <li>Profile</li>
           <li></li>
           <li></li>
           <li></li>
           <li></li>
         </ul>
       </nav>
+      <div className="">Main content</div>
     </div>
   );
 }
