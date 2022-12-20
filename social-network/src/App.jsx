038a1b3dd6247980086a1dd6b26a78585dc89634
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-20 20:33:55 +0400

    structure-main-page-added

--- social-network/src/App.jsx
+++ social-network/src/App.jsx
@@ -1,21 +1,37 @@
 import "./App.css";
 import logo from "./img/header/logo.svg";
+import bird from "./img/mainContent/bird.jpg";
+import avatar from "./img/mainContent/avatar.png";
+
 function App() {
   return (
     <div className="app-wrapper">
-      <header className="App-header">
-        <img src={logo} width="24" height="24" />
+      <header className="header">
+        <img src={logo} width="24" height="24" alt="logo" />
       </header>
-      <nav>
+      <nav className="nav">
         <ul>
           <li>Profile</li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
+          <li>Messages</li>
+          <li>News</li>
+          <li>Music</li>
+          <li>Setting</li>
         </ul>
       </nav>
-      <div className="">Main content</div>
+      <div className="content">
+        <div className="">
+          <img src={bird} width="100%" height="800" alt="content" />
+        </div>
+        <div className="avatarAndPosts">
+          <img src={avatar} alt="avatar" />
+          <div className="description">description</div>
+        </div>
+        <div className="myPosts">
+          <div className="newPost">New post</div>
+          <div className="post1">Post1</div>
+          <div className="post2">Post2</div>
+        </div>
+      </div>
     </div>
   );
 }
