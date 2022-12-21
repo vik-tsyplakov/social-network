27c3e89e8a389c86723331c66725ff52a5c1e7a9
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-21 17:26:12 +0400

    component-Profile-created

--- social-network/src/App.jsx
+++ social-network/src/App.jsx
@@ -1,39 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
-import bird from "./img/mainContent/bird.jpg";
-import avatar from "./img/mainContent/avatar.png";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="app-wrapper">
       <Header />
       <Navbar />
-      <div className="content">
-        <div>
-          <img src={bird} className="imgContent" alt="content" />
-        </div>
-        <div className="avatarAndPosts">
-          <img src={avatar} className="avatar" alt="avatar" />
-          <div className="description">
-            <p>Name Surname</p>
-            <p>Date of Birth: 2 january</p>
-            <p>City: Kyiv</p>
-            <p>Education: KPI'18</p>
-            <p>
-              Web Site:
-              <a href="https://github.com/vik-tsyplakov">
-                github.com/vik-tsyplakov
-              </a>
-            </p>
-          </div>
-        </div>
-        <div className="myPosts">
-          <div className="newPost">New post</div>
-          <div className="post1">Post1</div>
-          <div className="post2">Post2</div>
-        </div>
-      </div>
+      <Profile />
     </div>
   );
 }
---
+++ social-network/src/components/Profile.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import bird from "./../img/mainContent/bird.jpg";
+import avatar from "./../img/mainContent/avatar.png";
+
+export default function Profile() {
+  return (
+    <div className="content">
+      <div>
+        <img src={bird} className="imgContent" alt="content" />
+      </div>
+      <div className="avatarAndPosts">
+        <img src={avatar} className="avatar" alt="avatar" />
+        <div className="description">
+          <p>Name Surname</p>
+          <p>Date of Birth: 2 january</p>
+          <p>City: Kyiv</p>
+          <p>Education: KPI'18</p>
+          <p>
+            Web Site:
+            <a href="https://github.com/vik-tsyplakov">
+              github.com/vik-tsyplakov
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="myPosts">
+        <div className="newPost">New post</div>
+        <div className="post1">Post1</div>
+        <div className="post2">Post2</div>
+      </div>
+    </div>
+  );
+}
