734aceb1de12a7f28f6c530083992952e8d81c07
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-21 13:43:02 +0400

    structure-description-added

--- social-network/src/App.jsx
+++ social-network/src/App.jsx
@@ -19,12 +19,23 @@ function App() {
         </ul>
       </nav>
       <div className="content">
-        <div className="">
-          <img src={bird} width="100%" height="800" alt="content" />
+        <div>
+          <img src={bird} className="imgContent" alt="content" />
         </div>
         <div className="avatarAndPosts">
-          <img src={avatar} alt="avatar" />
-          <div className="description">description</div>
+          <img src={avatar} className="avatar" alt="avatar" />
+          <div className="description">
+            <p>Name Surname</p>
+            <p>Date of Birth: 2 january</p>
+            <p>City: Kyiv</p>
+            <p>Education: KPI'18</p>
+            <p>
+              Web Site:
+              <a href="https://github.com/vik-tsyplakov">
+                github.com/vik-tsyplakov
+              </a>
+            </p>
+          </div>
         </div>
         <div className="myPosts">
           <div className="newPost">New post</div>
