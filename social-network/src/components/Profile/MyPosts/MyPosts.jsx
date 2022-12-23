e5849a441f6887a1c63ab3cd43bba1d954f2abac
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-23 20:24:13 +0400

    Post-added-props-message

--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,7 +6,10 @@ export default function MyPosts() {
   return (
     <div className="myPosts">
       <div className="newPost">New post</div>
-      <Post />
+      <Post message="It is my first post" />
+      <Post message="I am 28 years old" />
+      <Post message="I am learning React" />
+      <Post message="How are you?" />
     </div>
   );
 }
--- social-network/src/components/Profile/MyPosts/Post/Post.jsx
+++ social-network/src/components/Profile/MyPosts/Post/Post.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import "./Post.css";
 import avatar from "./../../../../img/mainContent/avatar.png";
 
-export default function Post() {
+export default function Post(props) {
   return (
     <div className="post">
       <img className="post__avatar" src={avatar} alt="user photo" />
-      <p className="post__text">Post1</p>
+      <p className="post__text">{props.message}</p>
     </div>
   );
 }
