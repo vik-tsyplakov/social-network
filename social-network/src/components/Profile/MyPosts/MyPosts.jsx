cd46dc00e6ba3cac21a3421abb9169c3ce81b466
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2022-12-24 13:12:25 +0400

    likesCount-added-at-Post

--- social-network/src/components/Profile/MyPosts/MyPosts.jsx
+++ social-network/src/components/Profile/MyPosts/MyPosts.jsx
@@ -6,10 +6,10 @@ export default function MyPosts() {
   return (
     <div className="myPosts">
       <div className="newPost">New post</div>
-      <Post message="It is my first post" />
-      <Post message="I am 28 years old" />
-      <Post message="I am learning React" />
-      <Post message="How are you?" />
+      <Post message="It is my first post" likesCount={12} />
+      <Post message="I am 28 years old" likesCount={5} />
+      <Post message="I am learning React" likesCount={2} />
+      <Post message="How are you?" likesCount={0} />
     </div>
   );
 }
--- social-network/src/components/Profile/MyPosts/Post/Post.jsx
+++ social-network/src/components/Profile/MyPosts/Post/Post.jsx
@@ -6,7 +6,10 @@ export default function Post(props) {
   return (
     <div className="post">
       <img className="post__avatar" src={avatar} alt="user photo" />
-      <p className="post__text">{props.message}</p>
+      <div className="post__container">
+        <p className="post__text">{props.message}</p>
+        <p className="post__likesCount">like {props.likesCount}</p>
+      </div>
     </div>
   );
 }
