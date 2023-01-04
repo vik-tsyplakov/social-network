19bfe9126f43b1016add88583571b247e428d24e
vik-tsyplakov <100240020+vik-tsyplakov@users.noreply.github.com>
2023-01-04 18:28:08 +0400

    refactoring-mini-OOP

--- social-network/src/index.js
+++ social-network/src/index.js
@@ -1,15 +1,8 @@
 import "./index.css";
-import state from "./redux/state";
 import React from "react";
+import store from "./redux/state";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import {
-  addPost,
-  updateNewPostText,
-  addMessage,
-  updateMessageText,
-  subscribe,
-} from "./redux/state";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 let rerenderEntireTree = (state) => {
@@ -17,13 +10,13 @@ let rerenderEntireTree = (state) => {
     <React.StrictMode>
       <App
         state={state}
-        addPost={addPost}
-        updateNewPostText={updateNewPostText}
-        addMessage={addMessage}
-        updateMessageText={updateMessageText}
+        addPost={store.addPost.bind(store)}
+        updateNewPostText={store.updateNewPostText.bind(store)}
+        addMessage={store.addMessage.bind(store)}
+        updateMessageText={store.updateMessageText.bind(store)}
       />
     </React.StrictMode>
   );
 };
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
--- social-network/src/redux/state.js
+++ social-network/src/redux/state.js
@@ -1,100 +1,98 @@
-let rerenderEntireTree = () => {
-  console.log("State changed");
-};
-// Functions for posts
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    message: state.profilePage.newPostText,
-    likesCount: 0,
-  };
-  state.profilePage.postsData.push(newPost);
-  rerenderEntireTree(state);
-};
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-  rerenderEntireTree(state);
-};
-
-// Functions for messages
-export const addMessage = () => {
-  let newMessage = {
-    id: 7,
-    message: state.dialogsPage.newMessageText,
-  };
-  state.dialogsPage.messagesData.push(newMessage);
-  rerenderEntireTree(state);
-};
-
-export const updateMessageText = (newMessage) => {
-  state.dialogsPage.newMessageText = newMessage;
-  rerenderEntireTree(state);
-};
-
-export const subscribe = (observer) => {
-  rerenderEntireTree = observer;
-};
-// Data
-let state = {
-  profilePage: {
-    postsData: [
-      { id: 1, message: "It is my first post", likesCount: "12" },
-      { id: 2, message: "I am 28 years old", likesCount: "5" },
-      { id: 3, message: "I am learning React", likesCount: "2" },
-      { id: 4, message: "How are you?", likesCount: "0" },
-    ],
-    newPostText: "post",
+let store = {
+  _state: {
+    profilePage: {
+      postsData: [
+        { id: 1, message: "It is my first post", likesCount: "12" },
+        { id: 2, message: "I am 28 years old", likesCount: "5" },
+        { id: 3, message: "I am learning React", likesCount: "2" },
+        { id: 4, message: "How are you?", likesCount: "0" },
+      ],
+      newPostText: "post",
+    },
+    dialogsPage: {
+      dialogsData: [
+        {
+          id: 1,
+          name: "Nick",
+          photo:
+            "https://images.pexels.com/photos/1812634/pexels-photo-1812634.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          id: 2,
+          name: "John",
+          photo:
+            "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          id: 3,
+          name: "Ivan",
+          photo:
+            "https://images.pexels.com/photos/1054048/pexels-photo-1054048.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          id: 4,
+          name: "Anna",
+          photo:
+            "https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          id: 5,
+          name: "Alex",
+          photo:
+            "https://images.pexels.com/photos/2748242/pexels-photo-2748242.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+        {
+          id: 6,
+          name: "Harry",
+          photo:
+            "https://images.pexels.com/photos/7561911/pexels-photo-7561911.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        },
+      ],
+      messagesData: [
+        { id: 1, message: "Hi!" },
+        { id: 2, message: "What`s up?" },
+        { id: 3, message: "Where are you?" },
+        { id: 4, message: "Ok" },
+        { id: 5, message: "Have a good day!" },
+        { id: 6, message: "I like it" },
+      ],
+      newMessageText: "",
+    },
   },
-  dialogsPage: {
-    dialogsData: [
-      {
-        id: 1,
-        name: "Nick",
-        photo:
-          "https://images.pexels.com/photos/1812634/pexels-photo-1812634.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      },
-      {
-        id: 2,
-        name: "John",
-        photo:
-          "https://images.pexels.com/photos/2380794/pexels-photo-2380794.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      },
-      {
-        id: 3,
-        name: "Ivan",
-        photo:
-          "https://images.pexels.com/photos/1054048/pexels-photo-1054048.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      },
-      {
-        id: 4,
-        name: "Anna",
-        photo:
-          "https://images.pexels.com/photos/1520760/pexels-photo-1520760.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      },
-      {
-        id: 5,
-        name: "Alex",
-        photo:
-          "https://images.pexels.com/photos/2748242/pexels-photo-2748242.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      },
-      {
-        id: 6,
-        name: "Harry",
-        photo:
-          "https://images.pexels.com/photos/7561911/pexels-photo-7561911.jpeg?auto=compress&cs=tinysrgb&w=1600",
-      },
-    ],
-    messagesData: [
-      { id: 1, message: "Hi!" },
-      { id: 2, message: "What`s up?" },
-      { id: 3, message: "Where are you?" },
-      { id: 4, message: "Ok" },
-      { id: 5, message: "Have a good day!" },
-      { id: 6, message: "I like it" },
-    ],
-    newMessageText: "",
+  getState() {
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log("State changed");
+  },
+  addPost() {
+    let newPost = {
+      id: 5,
+      message: this._state.profilePage.newPostText,
+      likesCount: 0,
+    };
+    this._state.profilePage.postsData.push(newPost);
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+  },
+  addMessage() {
+    let newMessage = {
+      id: 7,
+      message: this._state.dialogsPage.newMessageText,
+    };
+    this._state.dialogsPage.messagesData.push(newMessage);
+    this._callSubscriber(this._state);
+  },
+  updateMessageText(newMessage) {
+    this._state.dialogsPage.newMessageText = newMessage;
+    this._callSubscriber(this._state);
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
   },
 };
 
-export default state;
+export default store;
